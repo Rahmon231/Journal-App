10d1d5781c8fe529aa91a60fbb6be7ce2006334a
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-16 11:48:49 +0100

    journal list ui

--- app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
+++ app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
@@ -1,14 +1,20 @@
 package com.lemzeeyyy.journalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JournalListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TextView noPostTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+        recyclerView = findViewById(R.id.recyclerView);
+        noPostTV = findViewById(R.id.list_no_post);
     }
 }
