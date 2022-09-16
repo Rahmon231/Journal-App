15133b821cc5b71e88f53170f234d042e3ce3d29
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-16 13:17:44 +0100

    journal list activity

--- app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
+++ app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
@@ -1,10 +1,14 @@
 package com.lemzeeyyy.journalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -48,5 +52,35 @@ public class JournalListActivity extends AppCompatActivity {
         //PostArrayList
         journalList = new ArrayList<>();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                if(user!=null && firebaseAuth!=null){
+                    startActivity(new Intent(
+                            JournalListActivity.this,
+                            AddJournalActivity.class));
+                }
+                break;
+
+            case R.id.sign_out:
+                if(user!=null && firebaseAuth!=null){
+                    firebaseAuth.signOut();
+                    startActivity(new Intent(
+                            JournalListActivity.this,
+                            MainActivity.class
+                    ));
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
