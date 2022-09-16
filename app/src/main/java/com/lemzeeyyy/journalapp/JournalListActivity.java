ebc61269f90b6e8b6b25db581578427804d58853
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-16 12:27:47 +0100

    menu item

--- app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
+++ app/src/main/java/com/lemzeeyyy/journalapp/JournalListActivity.java
@@ -1,20 +1,52 @@
 package com.lemzeeyyy.journalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+import com.lemzeeyyy.journalapp.model.Journal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JournalListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TextView noPostTV;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+    private List<Journal> journalList;
+    private CollectionReference collectionReference = database.collection("Journal");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+
+        //Widgets
         recyclerView = findViewById(R.id.recyclerView);
         noPostTV = findViewById(R.id.list_no_post);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+
+
+        //PostArrayList
+        journalList = new ArrayList<>();
+
     }
 }
